7badbdde02e3356ea91a389687f0cd23f5df1bb5
raphaelsack <i20029@hb.dhbw-stuttgart.de>
2021-10-07 18:39:05 +0200

    day1 solutions

--- day1/hero_journey_one.cpp
+++ day1/hero_journey_one.cpp
@@ -12,7 +12,17 @@
 * The return value is the position of the element in the vector.
 */
 int ShowHeroSelect(std::vector<std::string> &heroes);
+int ShowHeroSelect(std::vector<std::string> &heroes){
+    std::cout << "Choose your hero: ";
+    for(int i = 0; i < heroes.size(); i++){
+        std::cout << i << ": " << heroes[i] << std::endl;
+    }
+    int choice;
+    std::cin >> choice;
+    std::cout << "========================" << std::endl;
 
+    return choice;
+}
 /**
  * Displays the initial menu, which gives you the possibility to
  * a) Create a new Hero
@@ -20,25 +30,66 @@ int ShowHeroSelect(std::vector<std::string> &heroes);
  * c) close the application
  */
 int ShowStartMenuSelect();
+int ShowStartMenuSelect(){
+    std::cout << "Please select an option:" << std::endl;
+    std::cout << "1) Create a new Hero" << std::endl;
+    std::cout << "2) Start a new game by selecting a hero" << std::endl;
+    std::cout << "3) Close the application" << std::endl;
+    int selection;
+    std::cin >> selection;
+    std::cout << "========================" << std::endl;
 
+    return selection;
+}
 /**
  * Creates a new Hero, which is nothing more but a string cointaining 
  * it's name , and add it to the inserted vector of strings
  * 
  */
 void CreateAndAddHero(std::vector<std::string> &heroes);
-
+void CreateAndAddHero(std::vector<std::string> &heroes){
+    std::string name;
+    std::cout << "Please enter the name of your hero: ";
+    std::cin >> name;
+    std::cout << "========================" << std::endl;
+    heroes.push_back(name);
+}
 /**
  * Displays a message containing the hero's name,
  * which tells about the encouraging adventures in the DLC
  * 
  */
 void ShowStartDialog(std::string &hero);
+void ShowStartDialog(std::string &hero){
+    std::cout << "Welcome to the game " << hero << "!" << std::endl;
+    std::cout << "You have been chosen to go on an adventure!" << std::endl;
+    std::cout << "You have been given the task to find the legendary sword, " << std::endl;
+    std::cout << "which is said to be hidden in the depths of the dungeon." << std::endl;
+    std::cout << "You must find it before the evil wizard can destroy it!" << std::endl;
+    std::cout << "Good luck!" << std::endl;
+    std::cout << "========================" << std::endl;
+}
 
 /**
  * Program to run
  */
 int main(){
-    printf("hello world");
+    std::vector<std::string> heroes;
+    int selection;
+    while(selection != 2){
+    selection = ShowStartMenuSelect();
+    switch(selection){
+        case 1:
+            CreateAndAddHero(heroes);
+            break;
+        case 2:
+            ShowStartDialog(heroes[ShowHeroSelect(heroes)]);
+            break;
+        case 3:
+            return 0;
+        default:
+            std::cout << "Invalid selection" << std::endl;
+            break;
+    }}
 }
 

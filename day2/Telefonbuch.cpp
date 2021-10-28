7ff4d487f40a60e7557dfcfb31ea4055584afb79
raphaelsack <i20029@hb.dhbw-stuttgart.de>
2021-10-28 08:35:22 +0200

    Aufgabe 3

--- day2/Telefonbuch.cpp
+++ day2/Telefonbuch.cpp
@@ -1,47 +1,202 @@
 #include <iostream>
 #include <vector>
-
-typedef struct en{
-    std::string name;
+#include <fstream>
+#include <string>   
+#include <functional>
+#define PRINTLINE std::cout << "----------------------------------------" << std::endl;
+typedef struct Entry
+{
     std::string surname;
+    std::string name;
     std::string telephone;
-    std::string email;
-} Entry;
-typedef struct Telefonbuch{
+} Entry_t;
+
+typedef struct Telefonbuch
+{
     std::vector<Entry *> entries;
-};
+} Telefonbuch_t;
 
-Entry searchEntry(Telefonbuch *tb, std::string name);
-void addEntry(Telefonbuch *tb, std::string name, std::string surname, std::string number, std::string email) ;
+void startUI(Telefonbuch_t *tb);
+Entry *searchEntry(Telefonbuch *tb, std::string surname);
+Entry *searchEntryByNumber(Telefonbuch *tb, std::string telephone);
+void addEntry(Telefonbuch *tb, std::string surname, std::string name, std::string telephone);
+int showMenu(Telefonbuch *tb);
+void loadEntries(Telefonbuch *tb, std::string filename);
+void saveEntries(Telefonbuch *tb, std::string filename);
+void split(const std::string &s, char c,std::vector<std::string> &v);
 
-int main(){
-    Telefonbuch tb;
-    addEntry(&tb, "Max", "Mustermann", "0123456789", "max.mustermann@muster.com");
-    addEntry(&tb, "hhh", "ghddgshgsd", "2904652", "rprhi@khk.de");
-    addEntry(&tb, "Raphael", "Sack", "0123456789", "i20029@hb.dhbw-stuttgart.de");
-
-    std::cout << searchEntry(&tb, "Max").telephone << std::endl;
-
-    
-
-
-
+int main()
+{
+    Telefonbuch_t *tb = new Telefonbuch_t;
+    startUI(tb);
+    delete tb;
 }
 
-Entry searchEntry(Telefonbuch *tb, std::string name){
-    for(int i = 0; i < tb->entries.size(); i++){
-        if(tb->entries[i]->name == name){
-            return *tb->entries[i];
+void loadEntries(Telefonbuch *tb, std::string filename){
+    std::ifstream file(filename);
+    std::string line;
+    while(std::getline(file, line)){
+        std::vector<std::string> v;
+        split(line, ';', v);
+        addEntry(tb, v[0], v[1], v[2]);
+    }
+}
+
+void saveEntries(Telefonbuch *tb, std::string filename)
+{
+    std::ofstream file;
+    file.open(filename);
+    for (int i = 0; i < tb->entries.size(); i++)
+    {
+        file << tb->entries[i]->surname << ";" << tb->entries[i]->name << ";" << tb->entries[i]->telephone << std::endl;
+    }
+}
+
+int showMenu(Telefonbuch *tb)
+{
+    PRINTLINE
+    std::cout << "1. Suche nach Namen" << std::endl;
+    std::cout << "2. Suche nach Telefonnummer" << std::endl;
+    std::cout << "3. Neuer Eintrag" << std::endl;
+    std::cout << "4. Alle anzeigen" << std::endl;
+    std::cout << "5. Beenden" << std::endl;
+    PRINTLINE
+    int choice;
+    std::cin >> choice;
+    switch (choice)
+    {
+    case 1:
+    {
+        std::cout << "Suche nach Namen:" << std::endl;
+        std::cout << "Nachname: ";
+        // Read in the surname and search tb for it
+        std::string surname;
+        std::cin >> surname;
+        Entry *e = searchEntry(tb, surname);
+        if (e != NULL)
+        {
+            PRINTLINE
+            std::cout << "Gefundener Eintrag:" << std::endl;
+            std::cout << e->surname << " " << e->name << " " << e->telephone << std::endl;
+        }
+        else
+        {
+            std::cout << "Eintrag nicht gefunden" << std::endl;
         }
     }
-    return *new Entry();
+    break;
+    case 2:
+    {
+        std::cout << "Suche nach Telefonnummer:" << std::endl;
+        // Read in the telephone number and search tb for it
+        std::string telephone;
+        std::cout << "Telefonnummer: ";
+        std::cin >> telephone;
+        Entry *e = searchEntryByNumber(tb, telephone);
+        if (e != NULL)
+        {
+            std::cout << "Gefundener Eintrag:" << std::endl;
+            std::cout << e->surname << " " << e->name << " " << e->telephone << std::endl;
+        }
+        else
+        {
+            std::cout << "Eintrag nicht gefunden" << std::endl;
+        }
+    }
+    break;
+    case 3:
+    {
+        std::cout << "Neuer Eintrag:" << std::endl;
+        // Read in the surname, name and telephone number and add it to tb
+        std::string surname;
+        std::cout << "Vorname: ";
+        std::cin >> surname;
+        std::string name;
+        std::cout << "Nachname: ";
+        std::cin >> name;
+        std::string telephone;
+        std::cout << "Telefonnummer: ";
+        std::cin >> telephone;
+        addEntry(tb, surname, name, telephone);
+    }
+    break;
+    case 4:
+    {
+        // Print all entries in tb
+        for (int i = 0; i < tb->entries.size(); i++)
+        {
+            std::cout << tb->entries[i]->surname << " " << tb->entries[i]->name << " " << tb->entries[i]->telephone << std::endl;
+        }
+    }
+    break;
+    case 5:
+    {
+        std::cout << "Beenden" << std::endl;
+        return 1;
+    }
+    break;
+    default:
+    {
+        std::cout << "Falsche Eingabe" << std::endl;
+        exit(0);
+        break;
+    }
+    }
+    return 0;
 }
 
-void addEntry(Telefonbuch *tb, std::string name, std::string surname, std::string number, std::string email) {
-    Entry e2e;
-    e2e.name = name;
-    e2e.email = email;
-    e2e.telephone = number;
-    e2e.surname = surname;
-    tb->entries.push_back(&e2e);
+void startUI(Telefonbuch_t *tb)
+{
+    loadEntries(tb, "Telefonbuch.txt");
+    while (std::cin.good() && !showMenu(tb))
+        ;
+    saveEntries(tb, "Telefonbuch.txt");
+}
+void addEntry(Telefonbuch *tb, std::string surname, std::string name, std::string telephone)
+{
+    Entry_t *e = new Entry_t;
+    e->surname = surname;
+    e->name = name;
+    e->telephone = telephone;
+    tb->entries.push_back(e);
+}
+
+Entry *searchEntry(Telefonbuch *tb, std::string surname)
+{
+    for (int i = 0; i < tb->entries.size(); i++)
+    {
+        if (tb->entries[i]->surname == surname)
+        {
+            return tb->entries[i];
+        }
+    }
+    return nullptr;
+}
+
+Entry *searchEntryByNumber(Telefonbuch *tb, std::string telephone)
+{
+    for (int i = 0; i < tb->entries.size(); i++)
+    {
+        if (tb->entries[i]->telephone == telephone)
+        {
+            return tb->entries[i];
+        }
+    }
+    return nullptr;
+}
+
+void split(const std::string &s, char c,std::vector<std::string> &v)
+{
+    std::string::size_type i = 0;
+    std::string::size_type j = s.find(c);
+
+    while (j != std::string::npos)
+    {
+        v.push_back(s.substr(i, j - i));
+        i = ++j;
+        j = s.find(c, j);
+
+        if (j == std::string::npos)
+            v.push_back(s.substr(i, s.length()));
+    }
 }
---
+++ day3/Task2/Sacramento.cpp
@@ -0,0 +1,196 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <cctype>
+#include <map>
+#include <algorithm>
+// a Struct that holds the information of a row from the file ../SacramentocrimeJanuary2006.csv
+typedef struct SacramentoRow
+{
+    std::string adress;
+    int district;
+    std::string beat;
+    std::string grid;
+    std::string crimedescr;
+    int ucr_ncic_code;
+    int year;
+    int month;
+    int day;
+    int hour;
+    int minute;
+    double latitude;
+    double longitude;
+} SacramentoRow_t;
+
+void loadCSV(std::string fileName, std::vector<SacramentoRow_t> &rows);
+void split(const std::string &s, char c, std::vector<std::string> &v);
+void createMostFrequentCrimesReport(std::vector<SacramentoRow_t> &rows);
+void createMostFrequentDistrictsReport(std::vector<SacramentoRow_t> &rows);
+void createMultipleAdressReport(std::vector<SacramentoRow_t> &rows);
+
+int main()
+{
+    std::vector<SacramentoRow_t> rows;
+    loadCSV(".\\sss.csv", rows);
+    std::cout << "---------------------------------------------" << std::endl;
+    std::cout << "Crimes Report, Sacramento 2006: " << std::endl;
+    std::cout << "Commited crimes in 2006: " << rows.size() << std::endl;
+    createMostFrequentCrimesReport(rows);
+    createMostFrequentDistrictsReport(rows);
+    createMultipleAdressReport(rows);
+    std::cout << "---------------------------------------------" << std::endl;
+
+    delete &rows;
+}
+
+void createMultipleAdressReport(std::vector<SacramentoRow_t> &rows)
+{
+
+    std::map<std::string, int> adressMap;
+    for (int i = 0; i < rows.size(); i++)
+    {
+        adressMap[rows[i].adress]++;
+    }
+    std::cout << "Multiple Adress Report:" << std::endl;
+    // Sorting-Alg, source: https://www.techiedelight.com/sort-map-values-cpp/
+    std::vector<std::pair<std::string, int>> vec;
+    std::copy(adressMap.begin(), adressMap.end(), std::back_inserter<std::vector<std::pair<std::string, int>>>(vec));
+    std::sort(vec.begin(), vec.end(),
+              [](const std::pair<std::string, int> &l, const std::pair<std::string, int> &r)
+              {
+                  if (l.second != r.second)
+                  {
+                      return l.second > r.second;
+                  }
+
+                  return l.first > r.first;
+              });
+
+    std::map<std::string, int>::iterator it;
+    for (auto const &pair: vec) {
+        if (pair.second == 1) break;
+        std::cout << pair.second << ": " << pair.first << std::endl;
+    }
+}
+
+void createMostFrequentDistrictsReport(std::vector<SacramentoRow_t> &rows)
+{
+    std::map<int, int> districts;
+    for (int i = 0; i < rows.size(); i++)
+    {
+        districts[rows[i].district]++;
+    }
+    std::map<int, int>::iterator it;
+    int max = 0;
+    int district;
+    for (std::map<int, int>::iterator it = districts.begin(); it != districts.end(); it++)
+    {
+        if (it->second > max)
+        {
+            max = it->second;
+            district = it->first;
+        }
+    }
+    std::cout << "The most dangerous district: " << district << " with " << max << " crimes" << std::endl;
+}
+
+void createMostFrequentCrimesReport(std::vector<SacramentoRow_t> &rows)
+{
+
+    std::map<std::string, int> crimeCount;
+    for (int i = 0; i < rows.size(); i++)
+    {
+        std::string crime = rows[i].crimedescr;
+        if (crimeCount.find(crime) == crimeCount.end())
+        {
+            crimeCount[crime] = 1;
+        }
+        else
+        {
+            crimeCount[crime]++;
+        }
+    }
+    
+    // Get the highest value in crimeCount
+    int max = 0;
+    std::string crime;
+    for (std::map<std::string, int>::iterator it = crimeCount.begin(); it != crimeCount.end(); it++)
+    {
+        if (it->second > max)
+        {
+            max = it->second;
+            crime = it->first;
+        }
+    }
+    std::cout << "The most frequent crime is: " << crime << " with " << max << " occurences" << std::endl;
+}
+
+void loadCSV(std::string fileName, std::vector<SacramentoRow_t> &rows)
+{
+    std::ifstream file;
+    file.open(fileName);
+    // std::cout << fileName << std::endl;
+    std::string line;
+    bool first = true;
+    if (file.is_open())
+    {
+        while (std::getline(file, line))
+        {
+            if (first)
+            {
+                first = false;
+                continue;
+            }
+            SacramentoRow_t *Srow = new SacramentoRow_t;
+            std::vector<std::string> v;
+            split(line, ',', v);
+            std::vector<std::string> datetime;
+            split(v[0], ' ', datetime);
+            std::vector<std::string> date;
+            std::vector<std::string> time;
+            split(datetime[0], '/', date);
+            split(datetime[1], ':', time);
+            v[3].erase(std::remove_if(v[3].begin(), v[3].end(), ::isspace), v[3].end());
+            Srow->month = std::stoi(date[0]);
+            Srow->day = std::stoi(date[1]);
+            Srow->year = std::stoi(date[2]);
+            Srow->hour = std::stoi(time[0]);
+            Srow->minute = std::stoi(time[1]);
+            Srow->adress = v[1];
+            Srow->district = std::stoi(v[2]);
+            if (v[3] == "")
+            {
+                Srow->beat = "N/A";
+            }
+            else
+            {
+                Srow->beat = std::stoi(v[3]);
+            }
+            Srow->beat = v[3];
+            Srow->grid = v[4];
+            Srow->crimedescr = v[5];
+            Srow->ucr_ncic_code = std::stoi(v[6]);
+            Srow->latitude = std::stod(v[7]);
+            Srow->longitude = std::stod(v[8]);
+            rows.push_back(*Srow);
+        }
+    }
+    file.close();
+}
+
+void split(const std::string &s, char c, std::vector<std::string> &v)
+{
+    std::string::size_type i = 0;
+    std::string::size_type j = s.find(c);
+
+    while (j != std::string::npos)
+    {
+        v.push_back(s.substr(i, j - i));
+        i = ++j;
+        j = s.find(c, j);
+
+        if (j == std::string::npos)
+            v.push_back(s.substr(i, s.length()));
+    }
+}
